56e3c08078968497627a21563e152cf612ceccb8
Josiah Holloway <josiah.holloway@tooeleschools.org>
2024-01-25 12:55:57 -0700

    Mario's Hat

--- Program.cs
+++ Program.cs
@@ -1,2 +1,41 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.Clear();
+Console.BackgroundColor = ConsoleColor.Blue;
+Console.SetCursorPosition(0,0);
+Console.WriteLine("                            ");
+Console.SetCursorPosition(0,1);
+Console.WriteLine("        ");
+Console.SetCursorPosition(18,1);
+Console.WriteLine("          ");
+Console.SetCursorPosition(0,2);
+Console.WriteLine("      ");
+Console.SetCursorPosition(24,2);
+Console.WriteLine("    ");
+Console.SetCursorPosition(0,3);
+Console.WriteLine("      ");
+
+Console.BackgroundColor = ConsoleColor.Red;
+Console.SetCursorPosition(8,1);
+Console.WriteLine("          ");
+Console.SetCursorPosition(6,2);
+Console.WriteLine("                  ");
+
+Console.BackgroundColor = ConsoleColor.Black;
+Console.SetCursorPosition(6,3);
+Console.WriteLine("      ");
+Console.SetCursorPosition(16,3);
+Console.WriteLine("  ");
+
+Console.BackgroundColor = ConsoleColor.Yellow;
+Console.SetCursorPosition(12,3);
+Console.WriteLine("    ");
+Console.SetCursorPosition(18,3);
+Console.WriteLine("  ");
+
+Console.BackgroundColor = ConsoleColor.White;
+Console.SetCursorPosition(12,3);
+Console.WriteLine("    ");
+Console.SetCursorPosition(18,3);
+Console.WriteLine("  ");
+
+
